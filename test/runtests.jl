b11493787bce18d4c7ccfda80f2680fef2a8724f
Johannes Feist <johannes.feist@gmail.com>
2020-10-28 00:40:16 +0100

    test with array instead of ntuple for julia 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,15 @@ using PredicateComposition
 using Test
 
 @testset "PredicateComposition.jl" begin
-    tstdata = ntuple(i -> (1:i).^2, 10)
-    @test filter(length ≺ 3, tstdata) == ([1],[1,4])
-    @test filter((length ⪰ 3) ⩓ (length ⪯ 5), tstdata) == ([1,4,9],[1,4,9,16],[1,4,9,16,25])
+    tstdata = [(1:i).^2 for i=1:10]
+    @test filter(length ≺ 3, tstdata) == [[1],[1,4]]
+    @test filter((length ⪰ 3) ⩓ (length ⪯ 5), tstdata) == [[1,4,9],[1,4,9,16],[1,4,9,16,25]]
 
     counteven(A) = count(iseven,A)
-    @test filter(MIN(length,counteven) ≺ 3, tstdata) == ([1],[1,4],[1,4,9],[1,4,9,16],[1,4,9,16,25])
+    @test filter(MIN(length,counteven) ≺ 3, tstdata) == [[1],[1,4],[1,4,9],[1,4,9,16],[1,4,9,16,25]]
 
-    @test map(SUM(length,counteven), tstdata) == (1, 3, 4, 6, 7, 9, 10, 12, 13, 15)
-    @test SUM(length,counteven).(tstdata) == (1, 3, 4, 6, 7, 9, 10, 12, 13, 15)
+    @test map(SUM(length,counteven), tstdata) == [1, 3, 4, 6, 7, 9, 10, 12, 13, 15]
+    @test SUM(length,counteven).(tstdata) == [1, 3, 4, 6, 7, 9, 10, 12, 13, 15]
 
     @test count(iseven ⩔ isodd, 1:100) == 100
     @test count(!iseven ⩓ !isodd, 1:100) == 0
