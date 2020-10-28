90ec06d2b9423c2146330cd7d688e87bd8d34408
Johannes Feist <johannes.feist@gmail.com>
2020-10-28 09:37:05 +0100

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,10 +7,23 @@ using Test
     @test filter((length ⪰ 3) ⩓ (length ⪯ 5), tstdata) == [[1,4,9],[1,4,9,16],[1,4,9,16,25]]
 
     counteven(A) = count(iseven,A)
+    countodd(A) = count(isodd,A)
     @test filter(MIN(length,counteven) ≺ 3, tstdata) == [[1],[1,4],[1,4,9],[1,4,9,16],[1,4,9,16,25]]
 
     @test map(SUM(length,counteven), tstdata) == [1, 3, 4, 6, 7, 9, 10, 12, 13, 15]
     @test SUM(length,counteven).(tstdata) == [1, 3, 4, 6, 7, 9, 10, 12, 13, 15]
+
+    @test filter(length ≣ counteven, tstdata) == []
+    @test filter(length ≺ counteven, tstdata) == []
+    @test filter(length ⪯ counteven, tstdata) == []
+    @test filter(length ≻ counteven, tstdata) == tstdata
+    @test filter(length ⪰ counteven, tstdata) == tstdata
+
+    @test filter(length ≣ countodd, tstdata) == [[1]]
+    @test filter(length ≺ countodd, tstdata) == []
+    @test filter(length ⪯ countodd, tstdata) == [[1]]
+    @test filter(length ≻ countodd, tstdata) == tstdata[2:end]
+    @test filter(length ⪰ countodd, tstdata) == tstdata
 
     @test count(iseven ⩔ isodd, 1:100) == 100
     @test count(!iseven ⩓ !isodd, 1:100) == 0
